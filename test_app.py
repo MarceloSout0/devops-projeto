3dc3d726b17dfc3be672ca481c7070cabaaa899e
MarceloSout0 <147437699+MarceloSout0@users.noreply.github.com>
2026-04-23 16:22:50 -0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,16 @@
+from app import soma, subtracao, multiplicacao, divisao
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+def test_subtracao():
+    assert subtracao(5, 3) == 2
+
+def test_multiplicacao():
+    assert multiplicacao(2, 3) == 6
+
+def test_divisao():
+    assert divisao(6, 2) == 3
+
+def test_divisao_decimal():
+    assert divisao(5, 2) == 2.5
